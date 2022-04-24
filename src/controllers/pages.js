478180130d51b07be58e64eaa198dcd075d00d6d
Sourav Kotkar <sourav.kotkar21@gmail.com>
2022-04-24 11:59:13 +0530

    added questions

--- src/controllers/pages.js
+++ src/controllers/pages.js
@@ -3,29 +3,82 @@ const User = require('../models/users');
 
 module.exports.start = async(req, res) => {
     const level = req.user.level;
-    res.redirect(`/page${level}`);
+    if(level==21){
+        req.flash("success", "All levels completed !");
+        res.redirect('/page0');
+    }
+    else{
+        res.redirect(`/page${level}`);
+    }
 }
-
 module.exports.renderPage0 = (req, res) => {
     res.render('page0');
 }
-
 module.exports.renderPage1 = (req, res) => {
     res.render('page1');
 }
-
 module.exports.renderPage2 = (req, res) => {
     res.render('page2');
 }
-
 module.exports.renderPage3 = (req, res) => {
     res.render('page3');
+}
+module.exports.renderPage4 = (req, res) => {
+    res.render('page4');
+}
+module.exports.renderPage5 = (req, res) => {
+    res.render('page5');
+}
+module.exports.renderPage6 = (req, res) => {
+    res.render('page6');
+}
+module.exports.renderPage7 = (req, res) => {
+    res.render('page7');
+}
+module.exports.renderPage8 = (req, res) => {
+    res.render('page8');
+}
+module.exports.renderPage9 = (req, res) => {
+    res.render('page9');
+}
+module.exports.renderPage10 = (req, res) => {
+    res.render('page10');
+}
+module.exports.renderPage11 = (req, res) => {
+    res.render('page11');
+}
+module.exports.renderPage12 = (req, res) => {
+    res.render('page12');
+}
+module.exports.renderPage13 = (req, res) => {
+    res.render('page13');
+}
+module.exports.renderPage14 = (req, res) => {
+    res.render('page14');
+}
+module.exports.renderPage15 = (req, res) => {
+    res.render('page15');
+}
+module.exports.renderPage16 = (req, res) => {
+    res.render('page16');
+}
+module.exports.renderPage17 = (req, res) => {
+    res.render('page17');
+}
+module.exports.renderPage18 = (req, res) => {
+    res.render('page18');
+}
+module.exports.renderPage19 = (req, res) => {
+    res.render('page19');
+}
+module.exports.renderPage20 = (req, res) => {
+    res.render('page20');
 }
 
 module.exports.submitPage1 = async(req, res) => {
     const answer = req.body.answer;
     console.log(answer);
-    if(answer == 'Male'){
+    if(answer == 'd'){
         await User.updateOne({"username": req.user.username}, {$set: {"level": 2, "score": 1}});
         req.flash("success", "Correct Answer !");
         res.redirect('/page2');
@@ -35,11 +88,10 @@ module.exports.submitPage1 = async(req, res) => {
         res.redirect('/page1');
     }
 }
-
 module.exports.submitPage2 = async(req, res) => {
     const answer = req.body.answer;
     console.log(answer);
-    if(answer == 'Boy'){
+    if(answer == '7'){
         await User.updateOne({"username": req.user.username}, {$set: {"level": 3, "score": 2}});
         req.flash("success", "Correct Answer !");
         res.redirect('/page3');
@@ -49,17 +101,237 @@ module.exports.submitPage2 = async(req, res) => {
         res.redirect('/page2');
     }
 }
-
 module.exports.submitPage3 = async(req, res) => {
     const answer = req.body.answer;
     console.log(answer);
-    if(answer == 'Sourav'){
+    if(answer == 'b'){
         await User.updateOne({"username": req.user.username}, {$set: {"level": 4, "score": 3}});
         req.flash("success", "Correct Answer !");
-        res.redirect('/page0');
+        res.redirect('/page4');
     }
     else{
         req.flash("error", "Wrong Answer !");
         res.redirect('/page3');
+    }
+}
+module.exports.submitPage4 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == '3'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 5, "score": 4}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page5');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page4');
+    }
+}
+module.exports.submitPage5 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == 'd'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 6, "score": 5}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page6');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page5');
+    }
+}
+module.exports.submitPage6 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == '2'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 7, "score": 6}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page7');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page6');
+    }
+}
+module.exports.submitPage7 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == 'a'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 8, "score": 7}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page8');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page7');
+    }
+}
+module.exports.submitPage8 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == '6'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 9, "score": 8}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page9');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page8');
+    }
+}
+module.exports.submitPage9 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == 'd'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 10, "score": 9}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page10');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page9');
+    }
+}
+module.exports.submitPage10 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == '35'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 11, "score": 10}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page11');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page10');
+    }
+}
+module.exports.submitPage11 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == 'c'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 12, "score": 11}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page12');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page11');
+    }
+}
+module.exports.submitPage12 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == '3'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 13, "score": 12}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page13');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page12');
+    }
+}
+module.exports.submitPage13 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == 'b'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 14, "score": 13}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page14');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page13');
+    }
+}
+module.exports.submitPage14 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == '3'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 15, "score": 14}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page15');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page14');
+    }
+}
+module.exports.submitPage15 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == 'c'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 16, "score": 15}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page16');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page15');
+    }
+}
+module.exports.submitPage16 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == '8'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 17, "score": 16}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page17');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page16');
+    }
+}
+module.exports.submitPage17 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == 'a'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 18, "score": 17}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page18');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page17');
+    }
+}
+module.exports.submitPage18 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == '511'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 19, "score": 18}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page19');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page18');
+    }
+}
+module.exports.submitPage19 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == 'b'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 20, "score": 19}});
+        req.flash("success", "Correct Answer !");
+        res.redirect('/page20');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page19');
+    }
+}
+module.exports.submitPage20 = async(req, res) => {
+    const answer = req.body.answer;
+    console.log(answer);
+    if(answer == '80'){
+        await User.updateOne({"username": req.user.username}, {$set: {"level": 21, "score": 20}});
+        req.flash("success", "Correct Answer ! All levels completed.");
+        res.redirect('/page0');
+    }
+    else{
+        req.flash("error", "Wrong Answer !");
+        res.redirect('/page20');
     }
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,8 @@ const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn } = require('../../middleware');
 const {renderHome, renderLeaderboard, renderLogin, loginUser, logoutUser, renderSignUp, SignUp} = require("../controllers/home");
-const {start, renderPage0, renderPage1, renderPage2, renderPage3, submitPage1, submitPage2, submitPage3} = require("../controllers/pages");
+const {start, renderPage0, renderPage1, renderPage2, renderPage3, renderPage4, renderPage5, renderPage6, renderPage7, renderPage8, renderPage9, renderPage10, renderPage11, renderPage12, renderPage13, renderPage14, renderPage15, renderPage16, renderPage17, renderPage18, renderPage19, renderPage20} = require("../controllers/pages");
+const {submitPage1, submitPage2, submitPage3, submitPage4, submitPage5, submitPage6, submitPage7, submitPage8, submitPage9, submitPage10,submitPage11, submitPage12, submitPage13, submitPage14, submitPage15, submitPage16, submitPage17, submitPage18, submitPage19, submitPage20} = require("../controllers/pages");
 
 //set up express router
 const router = express.Router();
@@ -24,10 +25,25 @@ router.route("/start").get(isLoggedIn, start);
 router.route("/page0").get(isLoggedIn, renderPage0);
 
 router.route("/page1").get(isLoggedIn, renderPage1).post(isLoggedIn, submitPage1);
-
 router.route("/page2").get(isLoggedIn, renderPage2).post(isLoggedIn, submitPage2);
-
 router.route("/page3").get(isLoggedIn, renderPage3).post(isLoggedIn, submitPage3);
+router.route("/page4").get(isLoggedIn, renderPage4).post(isLoggedIn, submitPage4);
+router.route("/page5").get(isLoggedIn, renderPage5).post(isLoggedIn, submitPage5);
+router.route("/page6").get(isLoggedIn, renderPage6).post(isLoggedIn, submitPage6);
+router.route("/page7").get(isLoggedIn, renderPage7).post(isLoggedIn, submitPage7);
+router.route("/page8").get(isLoggedIn, renderPage8).post(isLoggedIn, submitPage8);
+router.route("/page9").get(isLoggedIn, renderPage9).post(isLoggedIn, submitPage9);
+router.route("/page10").get(isLoggedIn, renderPage10).post(isLoggedIn, submitPage10);
+router.route("/page11").get(isLoggedIn, renderPage11).post(isLoggedIn, submitPage11);
+router.route("/page12").get(isLoggedIn, renderPage12).post(isLoggedIn, submitPage12);
+router.route("/page13").get(isLoggedIn, renderPage13).post(isLoggedIn, submitPage13);
+router.route("/page14").get(isLoggedIn, renderPage14).post(isLoggedIn, submitPage14);
+router.route("/page15").get(isLoggedIn, renderPage15).post(isLoggedIn, submitPage15);
+router.route("/page16").get(isLoggedIn, renderPage16).post(isLoggedIn, submitPage16);
+router.route("/page17").get(isLoggedIn, renderPage17).post(isLoggedIn, submitPage17);
+router.route("/page18").get(isLoggedIn, renderPage18).post(isLoggedIn, submitPage18);
+router.route("/page19").get(isLoggedIn, renderPage19).post(isLoggedIn, submitPage19);
+router.route("/page20").get(isLoggedIn, renderPage20).post(isLoggedIn, submitPage20);
 
 router.route("/").get(renderHome);
 
